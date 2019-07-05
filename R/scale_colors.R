5551610b15eca0899fb187caaed63b3bbcac4f4b
Daniel Marcelino <dmarcelino@live.com>
2019-07-05 09:28:39 -0300

    downgrade R version

--- R/scale_colors.R
+++ R/scale_colors.R
@@ -7,6 +7,40 @@
 # azul - #2ab2e3
 # verde - #3fa674
 # amarelo - #fed350
+
+agree_disagree_palette <- c("#2AB2E3", "#F05741","#B0B0B0")
+
+#' A muted, qualitative color palette
+#'
+#' @export
+#' @examples
+#' library(scales)
+#' scales::show_col(agree_disagree_pal()(4))
+agree_disagree_pal <- function() {scales::manual_pal(agree_disagree_palette) }
+
+#' Discrete color & fill scales based on the agree, disagree DK categories
+#'
+#' See [agree_disagree_pal]().
+#'
+#' @md
+#' @inheritDotParams ggplot2::discrete_scale -expand -position
+#' @rdname scale_agree_disagree
+#' @export
+scale_colour_agree_disagree <- function(...) { discrete_scale("colour", "agree_disagree", agree_disagree_pal(), ...) }
+
+#' @export
+#' @rdname scale_agree_disagree
+scale_color_agree_disagree <- scale_colour_agree_disagree
+
+#' @export
+#' @rdname scale_agree_disagree
+scale_fill_agree_disagree <- function(...) { discrete_scale("fill", "agree_disagree", agree_disagree_pal(), ...) }
+
+
+
+
+
+
 idealpoints_palette <- c("#F05741", "#2AB2E3","#B0B0B0")
 
 jota_palette <- c("#F05741", "#2AB2E3", "#3FA674", "#FED350")
@@ -36,6 +70,9 @@ scale_color_idealpoints <- scale_colour_idealpoints
 #' @export
 #' @rdname scale_idealpoints
 scale_fill_idealpoints <- function(...) { discrete_scale("fill", "idealpoints", idealpoints_pal(), ...) }
+
+
+
 
 
 
